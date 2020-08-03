048f3c394eedd057383d2e4a0b59db05b313e9ea
Natalya Civilyova <tsivilyova@magora-systems.com>
2020-08-03 13:55:56 +0400

    changed table

--- server/src/db/table/Cities.kt
+++ server/src/db/table/Cities.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
 object Cities : Table(){
-    var id: Column<Long> = Cities.long("city_id").autoIncrement().primaryKey()
+    var id: Column<Long> = Cities.long("id").autoIncrement().primaryKey()
     var name: Column<String> = Cities.text("name")
-    var weatherDegree: Column<Long> = Cities.long("weather_degree")
+    var weatherDegree: Column<Long> = Cities.long("weatherdegree")
 }
