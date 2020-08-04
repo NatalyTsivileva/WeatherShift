23cd8075c43412d962fe35733a8d4bb613cddd0d
Natalya Civilyova <tsivilyova@magora-systems.com>
2020-08-04 12:51:30 +0400

    added count of updated elem in server response

--- server/src/Application.kt
+++ server/src/Application.kt
@@ -68,9 +68,9 @@ fun Application.module(testing: Boolean = false) {
                 val data = call.receive<City>()
                 val updatedCount = repository.updateElem(data)
                 if (updatedCount > 0) {
-                    call.respond(HttpStatusCode.OK,updatedCount)
+                    call.respond(updatedCount)
                 } else {
-                    call.respond(HttpStatusCode.NotFound,0)
+                    call.respond(0)
                 }
             }
         }
