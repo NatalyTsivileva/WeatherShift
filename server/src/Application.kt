56064d8ed0c868c5a4744c97e865568876d26494
Tsivilyova Nata <tsivilyova@magora-systems.com>
2021-10-01 14:50:55 +0700

    try to reload project

--- server/src/Application.kt
+++ server/src/Application.kt
@@ -17,7 +17,7 @@ import java.net.URI
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
+@Suppress("unused")  // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) {
